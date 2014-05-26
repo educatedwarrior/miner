615bd57c27d18402c062db53e07fd7cafd9e0ff4
Educated Warrior <educatedwarrior@fusemail.com>
2014-05-26 01:54:08 -0400

    initial commit

--- manifests/profiles/gpuminer.pp
+++ manifests/profiles/gpuminer.pp
@@ -1,10 +1,38 @@
 class miner::profiles::gpuminer (	) {
 
-    $packages_dep1= ['libcurl4-openssl-dev','automake', 'pkg-config', 'libtool',]
+    $packages_dep1= ['libcurl4-openssl-dev','automake', 'pkg-config', 'libtool']
 
-# Install all depending packages so we are able to compile sgminer
+    $git_repository="https://github.com/prettyhatemachine/sph-sgminer.git"
+    $miner_dir="/root/sph-sgminer"
+
+    $ADL_SDK_file="ADL_SDK_6.0.zip"
+    $ADL_SDK_url="https://sites.google.com/site/cryptoculture/files/${ADL_SDK_file}"
+
+    # Install all depending packages so we are able to compile sgminer
     package {$packages_dep1:
         ensure => installed,
     }
+
+    # Clone mining software
+    exec { "clone_sgminer":
+        command => "/usr/bin/git clone $git_repository",
+        creates => $miner_dir,
+        #path    => "/usr/local/bin/:/bin/",
+        cwd =>    "/root"   ,
+        # path    => [ "/usr/local/bin/", "/bin/" ],  # alternative syntax
+        # require => [Package[$packages_dep1],Package[$packages_dep2]],
+    }
+
+    # Clone mining software
+    exec { "get_ADL_SDK":
+        command => "/usr/bin/wget ${ADL_SDK_url}",
+        creates => "/root/${ADL_SDK_file}",
+        #path    => "/usr/local/bin/:/bin/",
+        cwd =>    "/root"   ,
+        # path    => [ "/usr/local/bin/", "/bin/" ],  # alternative syntax
+        # require => [Package[$packages_dep1],Package[$packages_dep2]],
+    }
+
+
 
 }
